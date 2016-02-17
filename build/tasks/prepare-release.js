8d0743a10fb11174b78bcd4a3ba2d06720bafb80
Stephen Lautier <stephen.lautier@outlook.com>
2016-02-17 02:22:15 +0100

    chore(gulp):  implementing automated release process

--- build/tasks/prepare-release.js
+++ build/tasks/prepare-release.js
@@ -1,9 +1,13 @@
 var gulp = require("gulp");
 var runSequence = require("run-sequence");
-var paths = require("../paths");
 var conventionalChangelog = require("gulp-conventional-changelog");
 var bump = require("gulp-bump");
+var git = require("gulp-git");
+var fs = require("fs");
+
 var args = require("../args");
+var paths = require("../paths");
+
 
 gulp.task("bump-version", () => {
 	return gulp.src(["./package.json"])
@@ -33,4 +37,40 @@ gulp.task("prepare-release", (cb) => {
 		"changelog",
 		cb
 		);
+});
+
+gulp.task("perform-release", (cb) => {
+	return runSequence(
+		"commit-changes",
+		"push-changes",
+		"create-new-tag",
+		cb
+		);
+});
+
+
+gulp.task("commit-changes", () => {
+	return gulp.src(".")
+		.pipe(git.add())
+		.pipe(git.commit("[Prerelease] Bumped version number"));
+});
+
+gulp.task("push-changes", (cb) => {
+	git.push("origin", "master", cb);
+});
+
+gulp.task("create-new-tag", (cb) => {
+	var version = getPackageJsonVersion();
+	git.tag(version, `Created Tag for version: ${version}`, (error) => {
+		if (error) {
+			return cb(error);
+		}
+		git.push("origin", "master", { args: "--tags" }, cb);
+	});
+
+	function getPackageJsonVersion() {
+		// We parse the json file instead of using require because require caches
+		// multiple calls so the version number won't be updated
+		return JSON.parse(fs.readFileSync("./package.json", "utf8")).version;
+	}
 });
