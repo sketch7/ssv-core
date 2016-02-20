3288822fb1d324bd610e502358786c2949a2aa8d
Stephen Lautier <stephen.lautier@outlook.com>
2016-02-20 21:27:37 +0100

    feat(typings): modified typings to include devDep
    - jasmine stuff has been moved to ambientDevDependencies
    - jquery has been removed

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,4 +1,1 @@
-/// <reference path="browser\ambient\jasmine-es6-promise-matchers\jasmine-es6-promise-matchers.d.ts" />
-/// <reference path="browser\ambient\jasmine\jasmine.d.ts" />
-/// <reference path="browser\ambient\jquery\jquery.d.ts" />
 /// <reference path="browser\ambient\lodash\lodash.d.ts" />
