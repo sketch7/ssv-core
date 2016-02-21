2464eb79983788acc8b882e2375220358fadd55a
Stephen Lautier <stephen.lautier@outlook.com>
2016-02-21 19:49:49 +0100

    doc(string extensions): added documentation for interpolation

--- src/string.ts
+++ src/string.ts
@@ -1,5 +1,13 @@
 ﻿export class StringExtensions {
 
+	/**
+	 * Interpolates the string with the data specified by using the prefix (:) default as token.
+	 * e.g. 'api/:lang/games' => 'api/en/games'
+	 * @param {string} value string template to interpolate
+	 * @param {*} data data object to replace tokens
+	 * @param {string} [interpolatePrefix=":"] interpolation token prefix
+	 * @returns interpolated string
+	 */
 	interpolate(value: string, data: any, interpolatePrefix = ":") {
 		data = data || {};
 		let interpolatedValue = value.replace(/(\(\s*|\s*\)|\s*\|\s*)/g, "");
