b2b6fdb9d40c45c29c07438a6ab7f507bfb8cc03
Stephen Lautier <stephen.lautier@outlook.com>
2016-02-22 22:36:15 +0100

    chore(typings): added ambientDevDependencies  + fixed space issue

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,1 +1,3 @@
+/// <reference path="browser\ambient\jasmine-es6-promise-matchers\jasmine-es6-promise-matchers.d.ts" />
+/// <reference path="browser\ambient\jasmine\jasmine.d.ts" />
 /// <reference path="browser\ambient\lodash\lodash.d.ts" />
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,1 +1,3 @@
+/// <reference path="main\ambient\jasmine-es6-promise-matchers\jasmine-es6-promise-matchers.d.ts" />
+/// <reference path="main\ambient\jasmine\jasmine.d.ts" />
 /// <reference path="main\ambient\lodash\lodash.d.ts" />
