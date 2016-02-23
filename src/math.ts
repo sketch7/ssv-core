3730731eba20ea536272d2b5d45bdb9a8ba15daa
Stephen Lautier <stephen.lautier@outlook.com>
2016-02-24 00:33:56 +0100

    feat(math): implemented `round` functionality to math which supports both string and numeric

--- src/math.ts
+++ src/math.ts
@@ -1,8 +1,24 @@
-﻿export class MathExtensions {
+﻿
+/**
+ * Mathematics utilities and helpers.
+ * 
+ * @export
+ * @class MathExtensions
+ */
+export class MathExtensions {
 
-	// TODO: remove once finish testing.
-	add(value: number, valueB: number) {
-		return value + valueB;
+	/**
+	 * Rounds numeric value to a specified precision (default 2) e.g. 20.556 => 20.57.
+	 * @param value e.g. 20.556
+	 * @param precision e.g. 2 (default: 2)
+	 * @returns {20.56}
+	 */
+	round(value: string | number, precision = 2): number {
+		if (typeof value === "string") {
+			return Number(parseFloat(value).toFixed(precision));
+		} else {
+			return Number(value.toFixed(precision));
+		}
 	}
 
 }
