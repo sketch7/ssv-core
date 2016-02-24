21f1ca8657a9ac8321eaf99700696485a8f6f783
Stephen Lautier <stephen.lautier@outlook.com>
2016-02-24 01:31:53 +0100

    doc(math): minor improvements to doc for `round` function

--- src/math.ts
+++ src/math.ts
@@ -9,9 +9,9 @@ export class MathExtensions {
 
 	/**
 	 * Rounds numeric value to a specified precision (default 2) e.g. 20.556 => 20.57.
-	 * @param value e.g. 20.556
-	 * @param precision e.g. 2 (default: 2)
-	 * @returns {20.56}
+	 * @param value amount to round e.g. 20.556
+	 * @param precision decimal places to round to e.g. 2 (default: 2)
+	 * @returns {number} rounded number
 	 */
 	round(value: string | number, precision = 2): number {
 		if (typeof value === "string") {
