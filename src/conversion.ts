8d7f023d75ca54212529130a7b7cfb203a0041e3
Stephen Lautier <stephen.lautier@outlook.com>
2016-03-04 02:57:38 +0100

    doc(conversion): added method documentation

--- src/conversion.ts
+++ src/conversion.ts
@@ -14,30 +14,70 @@ export class ConversionExtensions {
 	SECONDS_PER_MINUTE = 60;
 	MILLIS_PER_MINUTE = this.MILLIS_PER_SECOND * this.SECONDS_PER_MINUTE;
 
+
+	/**
+	 * Converts from milliseconds to seconds e.g. 2500 (ms) => 2.5 (s)
+	 * @param {number} milliseconds value to convert e.g. 2500
+	 * @returns {number} time in seconds
+	 */
 	fromMillisecondsToSeconds(milliseconds: number): number {
 		return milliseconds / this.MILLIS_PER_SECOND;
 	}
 
+	/**
+	 * Converts from milliseconds to minutes e.g. 72000 (ms) => 1.2 (m)
+	 * @param {number} milliseconds value to convert e.g. 72000
+	 * @returns {number} time in minutes
+	 */
 	fromMillisecondsToMinutes(milliseconds: number): number {
 		return milliseconds / this.MILLIS_PER_MINUTE;
 	}
 
+	/**
+	 * Converts from seconds to milliseconds e.g. 2.5 (s) => 2500 (ms)
+	 * @param {number} seconds value to convert e.g. 2.5
+	 * @returns {number} time in milliseconds
+	 */
 	fromSecondsToMilliseconds(seconds: number): number {
 		return seconds * this.MILLIS_PER_SECOND;
 	}
 
+	/**
+	 * Converts from seconds to minutes e.g. 120 (s) => 2 (m)
+	 * @param {number} seconds value to convert e.g. 120
+	 * @returns {number} time in minutes
+	 */
 	fromSecondsToMinutes(seconds: number): number {
 		return seconds / this.SECONDS_PER_MINUTE;
 	}
 
+	/**
+	 * Converts from minutes to milliseconds e.g. 2 (m) => 120000 (m)
+	 * @param {number} seconds value to convert e.g. 2
+	 * @returns {number} time in milliseconds
+	 */
 	fromMinutesToMilliseconds(minutes: number): number {
 		return minutes * this.MILLIS_PER_MINUTE;
 	}
 
+	/**
+	 * Converts from minutes to seconds e.g. 2 (m) => 120 (s)
+	 * @param {number} seconds value to convert e.g. 2
+	 * @returns {number} time in seconds
+	 */
 	fromMinutesToSeconds(minutes: number): number {
 		return minutes * this.SECONDS_PER_MINUTE;
 	}
 
+
+	/**
+	 * Converts time from specified time unit to an other time unit.
+	 * 
+	 * @param {number} value value to convert e.g. 2
+	 * @param {TimeUnits} fromUnit units to convert time from
+	 * @param {TimeUnits} toUnit units to convert time to
+	 * @returns {number} time in specified units
+	 */
 	convertTime(value: number, fromUnit: TimeUnits, toUnit: TimeUnits): number {
 
 		switch (fromUnit) {
