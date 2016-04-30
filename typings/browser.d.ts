a0a66ec394c7fa5cac85f63b90e83cd99b30b864
Stephen Lautier <stephen.lautier@outlook.com>
2016-04-30 19:29:10 +0200

    chore(typings): updated typings packages

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,3 +1,3 @@
-/// <reference path="browser\ambient\jasmine-es6-promise-matchers\jasmine-es6-promise-matchers.d.ts" />
-/// <reference path="browser\ambient\jasmine\jasmine.d.ts" />
-/// <reference path="browser\ambient\lodash\lodash.d.ts" />
+/// <reference path="browser/ambient/jasmine-es6-promise-matchers/index.d.ts" />
+/// <reference path="browser/ambient/jasmine/index.d.ts" />
+/// <reference path="browser/ambient/lodash/index.d.ts" />
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,3 +1,3 @@
-/// <reference path="main\ambient\jasmine-es6-promise-matchers\jasmine-es6-promise-matchers.d.ts" />
-/// <reference path="main\ambient\jasmine\jasmine.d.ts" />
-/// <reference path="main\ambient\lodash\lodash.d.ts" />
+/// <reference path="main/ambient/jasmine-es6-promise-matchers/index.d.ts" />
+/// <reference path="main/ambient/jasmine/index.d.ts" />
+/// <reference path="main/ambient/lodash/index.d.ts" />
