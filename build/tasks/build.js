0d3cc22b270c2c1bb0d2e0272e6d2ad9039efcdf
Stephen Lautier <stephen.lautier@outlook.com>
2016-06-03 00:09:40 +0200

    chore(sourcemaps): added sourcemaps

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -33,36 +33,27 @@ gulp.task("compile:ts", () => {
 	const tsProject = getTscProject();
 	const tsResult = gulp.src([...paths.src.typings, paths.src.ts, `!${paths.src.testTs}`])
 		.pipe(plumber())
-	//.pipe(changed(paths.output.dist, { extension: ".js" }))
-		//.pipe(sourcemaps.init())
+		//.pipe(changed(paths.output.dist, { extension: ".js" }))
+		.pipe(sourcemaps.init())
 		.pipe(tsc(tsProject));
 
 	return merge([
 		tsResult.js
-			//.pipe(sourcemaps.write("."))
+			.pipe(sourcemaps.write("."))
 			.pipe(gulp.dest(`${paths.output.artifact}/amd`)),
-		tsResult.dts.pipe(gulp.dest(`${paths.output.artifact}/typings`))
-				]);
+		tsResult.dts
+			.pipe(gulp.dest(`${paths.output.artifact}/typings`))
+	]);
+});
+
+gulp.task("copy-dist", () => {
+	return gulp.src(`${paths.output.artifact}/**/*`)
+		.pipe(gulp.dest(`${paths.output.dist}`));
 });
 
 function getTscProject() {
 	return tsc.createProject("tsconfig.json", {
-		typescript: require("typescript"),
+		typescript: require("typescript")
 		//outFile: `${paths.packageName}.js`
 	});
 }
-
-// copy-dist
-gulp.task("copy-dist", () => {
-	return runSeq(["copy-dist:scripts", "copy-dist:dts"]);
-});
-
-gulp.task("copy-dist:scripts", () => {
-	return gulp.src(`${paths.output.artifact}/amd/**/*.js`)
-		.pipe(gulp.dest(`${paths.output.dist}/amd`));
-});
-
-gulp.task("copy-dist:dts", () => {
-	return gulp.src(`${paths.output.artifact}/typings/**/*.d.ts`)
-		.pipe(gulp.dest(`${paths.output.dist}/typings`));
-});
