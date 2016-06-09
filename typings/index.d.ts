97e5306efbba2ddef288b6873cdd8020483fccf8
Stephen Lautier <stephen.lautier@outlook.com>
2016-06-10 00:37:53 +0200

    chore(typings): updated lodash typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,3 @@
 /// <reference path="globals/jasmine-es6-promise-matchers/index.d.ts" />
 /// <reference path="globals/jasmine/index.d.ts" />
-/// <reference path="globals/lodash/index.d.ts" />
+/// <reference path="modules/lodash/index.d.ts" />
