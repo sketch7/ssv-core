69f265c4dce0bccc8112d931b88d7319caa15730
Stephen Lautier <stephen.lautier@outlook.com>
2016-11-14 23:38:11 +0100

    [Prepare Release] Bumped version to 0.7.2.

--- system.config.js
+++ system.config.js
@@ -44,7 +44,8 @@ System.config({
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "ieee754": "npm:ieee754@1.1.6",
       "isarray": "npm:isarray@1.0.0",
-      "process": "github:jspm/nodelibs-process@0.1.2"
+      "process": "github:jspm/nodelibs-process@0.1.2",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
     "npm:core-js@1.2.6": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
@@ -54,6 +55,9 @@ System.config({
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:isarray@1.0.0": {
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
     "npm:lodash@4.3.0": {
       "buffer": "github:jspm/nodelibs-buffer@0.1.0",
