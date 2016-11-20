54185fce13cd2766669739b2492f2692ae0b736d
Stephen Lautier <stephen.lautier@outlook.com>
2016-11-20 19:33:50 +0100

    style(index): reformatting

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 ﻿import * as collection from "./collection";
 import * as dateTime from "./date-time";
 import * as enumUtil from "./enum";
-import * as string from "./string";
 import * as math from "./math";
+import * as string from "./string";
 
 export * from "./keycode";
 
 export {
 	collection,
 	dateTime,
-	string,
 	enumUtil,
-	math
+	math,
+	string,
 }
