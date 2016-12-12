b04700f2fa3e4722543705cd4217d131b876728f
Stephen Lautier <stephen.lautier@outlook.com>
2016-12-12 17:17:50 +0100

    doc(enum): minor doc code styling

--- src/enum.ts
+++ src/enum.ts
@@ -3,16 +3,16 @@
 /**
  * Gets all names of the enum as an array.
  * @param {any} e enum to get data of.
- * @returns enum definition names as a string array e.g. ["Elite", "Boss", "Normal", "RaidBoss"].
+ * @returns enum definition names as a string array e.g. `["Elite", "Boss", "Normal", "RaidBoss"]`.
  */
 export function getNames(e: any): string[] {
 	return Object.keys(e).filter(v => isNaN(parseInt(v, 10)));
 }
 
 /**
- * Gets all names of the enum as an array with values kebab'ed cased characters e.g. "raid-boss"
+ * Gets all names of the enum as an array with values kebab'ed cased characters e.g. `"raid-boss"`
  * @param {any} e enum to get data of.
- * @returns enum definition names as a string array with kebab case e.g. ["elite", "boss", "normal", "raid-boss"].
+ * @returns enum definition names as a string array with kebab case e.g. `["elite", "boss", "normal", "raid-boss"]`.
  */
 export function getNamesKebab(e: any): string[] {
 	return Object.keys(e).map(v => _.kebabCase(v)).filter(v => isNaN(parseInt(v, 10)));
@@ -21,7 +21,7 @@ export function getNamesKebab(e: any): string[] {
 /**
  * Gets all values of the enum as an array.
  * @param {any} e enum to get data of.
- * @returns enum values as number array e.g. [1, 2, 3, 4]
+ * @returns enum values as number array e.g. `[1, 2, 3, 4]`
  */
 export function getValues(e: any): number[] {
 	return Object.keys(e).map(v => parseInt(v, 10)).filter(v => !isNaN(v));
