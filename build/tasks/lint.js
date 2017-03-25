4f12cd22175bc6f21b16ac28cead7c1dad260c7a
Stephen Lautier <stephen.lautier@outlook.com>
2017-03-25 21:42:46 +0100

    chore(packages): removed redudnant dev packages

--- build/tasks/lint.js
+++ build/tasks/lint.js
@@ -1,6 +1,4 @@
 const gulp = require("gulp");
-const tslint = require("gulp-tslint");
-const stylish = require("gulp-tslint-stylish");
 const ssvTools = require("@ssv/tools");
 
 const config = require("../config");
