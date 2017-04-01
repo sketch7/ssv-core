9d7ca4f2c1aa3a2676206586dc439cdebfeff0eb
Stephen Lautier <stephen.lautier@outlook.com>
2017-04-01 18:28:12 +0200

    feat(enum): added type constraint for enum
    and changed all enum utils to use it

--- src/enum.ts
+++ src/enum.ts
@@ -1,28 +1,35 @@
 ﻿import { kebabCase } from "lodash";
 
 /**
+ * Typing constraint for Enums (non `const`).
+ */
+export interface Enum {
+	[key: number]: string;
+}
+
+/**
  * Gets all names of the enum as an array.
- * @param {any} e enum to get data of.
+ * @param e enum to get data of.
  * @returns enum definition names as a string array e.g. `["Elite", "Boss", "Normal", "RaidBoss"]`.
  */
-export function getNames(e: any): string[] {
+export function getNames(e: Enum): string[] {
 	return Object.keys(e).filter(v => isNaN(parseInt(v, 10)));
 }
 
 /**
  * Gets all names of the enum as an array with values kebab'ed cased characters e.g. `"raid-boss"`
- * @param {any} e enum to get data of.
+ * @param e enum to get data of.
  * @returns enum definition names as a string array with kebab case e.g. `["elite", "boss", "normal", "raid-boss"]`.
  */
-export function getNamesKebab(e: any): string[] {
+export function getNamesKebab(e: Enum): string[] {
 	return Object.keys(e).filter(v => isNaN(parseInt(v, 10))).map(kebabCase);
 }
 
 /**
  * Gets all values of the enum as an array.
- * @param {any} e enum to get data of.
+ * @param e enum to get data of.
  * @returns enum values as number array e.g. `[1, 2, 3, 4]`
  */
-export function getValues(e: any): number[] {
+export function getValues(e: Enum): number[] {
 	return Object.keys(e).map(v => parseInt(v, 10)).filter(v => !isNaN(v));
 }
