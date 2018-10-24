6d91791adc6961009a1942c4889967ebf17267bf
Clayton Lautier <claylaut@hotmail.com>
2018-10-25 00:34:42 +0200

    fixed format

--- rollup.config.js
+++ rollup.config.js
@@ -5,12 +5,9 @@ const name = pkg.name.replace("@", "").replace(/(\/|\.)/g, "-");
 
 module.exports = {
 	input: pkg.module,
-	output: [{
-		file: pkg.main,
-		format: "umd",
-		name
-	},
-	{ file: pkg.module, format: "es", sourcemap: true }
+	output: [
+		{ file: pkg.main, format: "umd", name, sourcemap: true },
+		{ file: pkg.module, format: "es", sourcemap: true }
 	],
 	external: id => id.indexOf("node_modules") > -1,
 	onwarn
